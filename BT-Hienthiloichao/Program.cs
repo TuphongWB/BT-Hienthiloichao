9cf5c9c01c5662b03501518833983397c68ee4fa
Acer <Tuphong2611@gmail.com>
2023-05-17 08:49:01 +0700

    Add project files.

---
+++ BT-Hienthiloichao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_Hienthiloichao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello: " + yourName);
+            Console.ReadLine();
+        }
+    }
+}
